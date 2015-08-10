82e33f0a185fa438b2e0b167bfad986e3b237508
John Lander <john@linkodyssey.com>
2015-08-10 21:51:25 +0000

    Finish styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root 'welcome#index'
   get 'welcome/index'
   
   authenticated :user do
-    root 'notes#index', as:"authenticated_root"
+    root 'notes#index', as: 'authenticated_root'
   end
+  
+  root 'welcome#index'
   
   resources :notes
   
